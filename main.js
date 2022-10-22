75f4e70ae751a6620526e79bffe21f80b5565103
Moxie03 <oluwatobilobatolulope@gmail.com>
2022-10-22 09:54:08 +0100

    Antic project updated

---
+++ main.js
@@ -0,0 +1,8 @@
+
+var menuburger = document.querySelector(".menu-burgercontainer")
+var mobilelink = document.querySelector(".mobilelink-container")
+
+function addandremove(){
+    menuburger.classList.toggle('displaymenu')
+    mobilelink.classList.toggle('displaylinkbox')
+}
